efc4e1e2ae9f1ad63316094432d926e44c4110a8
Andre Kristensen <andrkri@ruc.dk>
2019-12-06 10:38:04 +0100

    Program med test

---
+++ Tests/sample/PaymentControllerTest.java
@@ -0,0 +1,42 @@
+package sample;
+
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.junit.Assert.*;
+
+public class PaymentControllerTest extends Payment {
+
+
+    Model m = Model.getInstance();
+    Connection conn;
+    Payment customerPayment = new Payment();
+
+
+    {
+        try {
+            conn = m.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void finalPayBtn() throws SQLException {
+
+        float newStoreBanlance = 10100;
+        customerPayment.executeUpdateBalance(newStoreBanlance, 2);
+        float storeResult = retrieveBalance(2);
+        assertEquals(storeResult, newStoreBanlance, 0);
+
+
+        float newCustomerBalance = 10100;
+        customerPayment.executeUpdateBalance(newCustomerBalance, 2);
+        float customerResult = retrieveBalance(2);
+        assertEquals(customerResult, newCustomerBalance, 0);
+
+
+    }
+}
